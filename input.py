71be4ad7ad10b397492948f677f0745ca057b77b
Stacie Widdifield <swiddifield111@gmail.com>
2017-01-06 14:52:26 -0700

    Add files via upload

---
+++ input.py
@@ -0,0 +1,26 @@
+#name = raw_input('What is your name? ')
+#print('Hi ' + name)
+
+#name = raw_input('What is your name?')
+#age = raw_input('How old are you?')
+#print(name + ' is ' + age + ' years old.')
+
+#age = raw_input('How old are you?')
+#dog_years = age * 7
+#print('You are ' + dog_years + 'old in dog years.')
+
+#age = raw_input('How old are you? ')
+#dog_years = int(age) * 7
+#print('You are ' + str(dog_years) + ' years old in dog years.')
+
+#meal_price = float(raw_input('How much was your meal? '))
+#tip = meal_price * .20
+#total_price = meal_price + tip
+#print('You should tip $' + str(tip) )
+#print('Your total cost would be' + str(total_price) )
+
+meal_price = float(raw_input('How much was your meal? '))
+tip = meal_price * .20
+total_price = meal_price + tip
+print('You should tip $' + str(tip) )
+print('Your total cost would be $' + str(total_price) )
