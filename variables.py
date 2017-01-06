37fc51ca1af85b0968b83c15f237e9a459506915
Stacie Widdifield <swiddifield111@gmail.com>
2017-01-06 11:45:09 -0700

    Practice joining variables together
    First step was to write each phrase a separate program, then put them all together. How do you put space between the end of once sentence and the next: not this: end.New but: end. New

---
+++ variables.py
@@ -0,0 +1,22 @@
+x = 4
+y = x + 1
+
+print(x)
+print(y)
+
+
+age = 30
+first_name = "Ariel"
+age = 30
+next_age = age + 1
+birth_month = 'August'
+birth_stone = 'peridot'
+
+print(first_name + ' is ' + str(age) + ' years old now. ') 
+
+print('But she will be ' + str(next_age) + ' in ' + birth_month  + '.')
+
+print ('Her birthstone is ' + birth_stone + '.')
+
+print(first_name + ' is ' + str(age) + ' years old now, but she will be ' + str(next_age) + ' in ' + birth_month  + '.' 'Her birthstone is ' + birth_stone + '.') 
+#How do I get the sentence that starts :"Her birthstone is.." to start one space after the period of the preceding sentence? I tried several things, but no go.
