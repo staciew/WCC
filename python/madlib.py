7ab166f34d5ebb130a8ed0b3a1cdeeb5e72e44fe
Stacie Widdifield <swiddifield111@gmail.com>
2017-01-08 13:02:31 -0700

    Done!
    Need help with spacing before and after input.

--- python/madlib.py
+++ python/madlib.py
@@ -35,3 +35,65 @@ name = 'Ani'
 beverage = 'chocolate milk'
 plural_body_part = 'knees'
 
+print('Welcome to the Mad Lib Generator')
+
+adjective1 = raw_input('Type in an adjective: ')
+
+adjective2 = raw_input('Another adjective: ')
+
+adjective3 = raw_input('And another adjective: ')
+
+adjective4 =  raw_input('One more adjective: ')
+
+
+plural_animal= raw_input('Plural animal (e.g., puppies) ')
+
+verb = raw_input('Now a verb: ')
+
+plural_noun = raw_input('Now a plural noun: ')
+
+
+name = raw_input('The first name of a woman musician: ')
+
+beverage = raw_input('A beverage: ')
+
+plural_body_part = raw_input('And finally, a plural body part: ')
+
+#adjective1 = 'fearless'
+#adjective2 = 'gooey'
+#adjective3 = 'sassy'
+#adjective4 = 'fancy'
+
+#plural_animal = 'elephants'
+#verb = 'skip'
+#plural_noun = 'marbles'
+
+#name = 'Ani'
+#beverage = 'chocolate milk'
+#plural_body_part = 'knees'
+
+print("\n") #error line
+print("Excellent choices! Here's your story!")
+print('_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _')
+print('There once was a' + adjective1 + 'programmer named'  + name )
+print('who wanted to build a' +adjective2 + 'mobile app')
+print( 'to help' + adjective3 +  plural_animal + 'find new homes.')
+
+print("\n") #error line
+print(name + 'stayed up all night, drinking lots of')
+print('caffeinated'  + beverage +  'as she worked and worked.')
+print('trying to complete her ' + adjective4 + 'program.')
+
+print("\n") #error line
+print('Whenever' + name + 'hit a dead end, she would')
+print ('exclaim *' + plural_noun + '*!')
+
+print("\n") #error line
+print('But she was not discouraged, and after a quick break')
+print('to' + verb + 'and clear her head, she got back to work.')
+
+print("\n") #error line
+print('By morning, when the sun started to rise, she let out a')
+print('big yawn and stretched her' + plural_body_part + '.')
+print('Finally, she was done.')
+print('- - - - - - - - - - - - - - - - - ')
